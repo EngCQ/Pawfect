256bdd8c9864346236fa1d95df4588cd49a504bb
EngCQ <engcherngqi@gmail.com>
2024-06-25 11:46:42 +0800

    Testing

--- flutter_assignment/lib/main.dart
+++ flutter_assignment/lib/main.dart
@@ -1,6 +1,18 @@
 import "package:flutter/material.dart";
+import "package:flutter_assignment/pet_adopters/header.dart";
 
-void main(){
-  runApp(const myApp);
+void main() {
+  runApp(const myApp());
 }
 
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Header(),
+    );
+  }
+}
